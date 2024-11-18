6258a12e40e77f0d3e82d4086c0d601aad80fac3
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-11-18 07:21:43 +0100

    Refreshing Tokens

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,6 +4,9 @@ import path from "path";
 import { Request, Response } from "express";
 import { comparePassword, encryptPassword } from '../config/encrypt';
 import jwt from "jsonwebtoken"
+import { config } from "dotenv";
+config();
+
 const userDb: UsersType = {
     users: require("../model/users.json"), setUsers: function (data) { this.users = data }
 }
@@ -15,18 +18,19 @@ export const handleLogin = async (req: Request, res: Response) => {
     }
     const foundUser = userDb.users.find(person => person.username === username)
     if (!foundUser) {
+        console.log("User not found")
         res.status(401).json({ error: "Invalid username or password" })
         return
     }
     const match = await comparePassword(password, foundUser.password || "")
     if (match) {
         const accessToken = jwt.sign({
-            username: foundUser.username
-        }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '1m' })
+            "username": foundUser.username
+        }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '5m' })
         const refreshToken = jwt.sign({
-            username: foundUser.username
-        }, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '3d' })
-        const otherUsers = userDb.users.filter(user => user.username != foundUser.username)
+            "username": foundUser.username
+        }, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '1d' })
+        const otherUsers = userDb.users.filter(user => user.username !== foundUser.username)
         const currentUser = { ...foundUser, refreshToken }
         userDb.setUsers([...otherUsers, currentUser])
         await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(userDb.users))
@@ -34,6 +38,7 @@ export const handleLogin = async (req: Request, res: Response) => {
         res.json({ "sucess": `User ${foundUser.username} is logged in`, accessToken })
     }
     else {
+        console.log("No password match found")
         res.sendStatus(401)
     }
 }
